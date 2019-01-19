4f9f0f66ae585d26c0f123db73ac9e899176ca2a
Kenzie Togami <ket1999@gmail.com>
2019-01-18 21:46:12 -0800

    GradleRIO-Redux 0.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.techshroom.inciseblue.commonLib
 
 plugins {
-    id("org.rivierarobotics.gradlerioredux") version "0.2.1"
+    id("org.rivierarobotics.gradlerioredux") version "0.3.0"
     id("net.ltgt.apt-idea") version "0.20"
     id("net.ltgt.apt-eclipse") version "0.20"
 }
