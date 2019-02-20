ffb379cfd8340b37aa294f226a4665978fd787bf
Kenzie Togami <ket1999@gmail.com>
2019-02-19 21:25:23 -0800

    Insert Talons into the shuffleboard

--- src/main/java/org/rivierarobotics/subsystems/TestControllers.java
+++ src/main/java/org/rivierarobotics/subsystems/TestControllers.java
@@ -3,7 +3,10 @@ package org.rivierarobotics.subsystems;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
+import edu.wpi.first.wpilibj.Sendable;
 import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +15,7 @@ public class TestControllers {
     private static final List<SpeedController> CONTROLLERS = new ArrayList<>();
 
     static {
+        ShuffleboardTab tab = Shuffleboard.getTab("Test Controllers");
         for (int i = 1; i <= 11; i++) {
             SpeedController c;
             switch (i) {
@@ -21,6 +25,7 @@ public class TestControllers {
                 case 10:
                 case 11:
                     c = new WPI_TalonSRX(i);
+                    tab.add((Sendable) c);
                     break;
                 default:
                     c = new CANSparkMax(i, CANSparkMaxLowLevel.MotorType.kBrushless);
