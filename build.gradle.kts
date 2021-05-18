c0b1b8d6c6d7c7afacf88566516ff138a25e35d0
Boomaa23 <cmo93003@yahoo.com>
2021-05-18 12:52:06 -0700

    Update to 2020 configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,23 @@
 import com.techshroom.inciseblue.commonLib
 
 plugins {
-    id("org.rivierarobotics.gradlerioredux") version "0.6.0"
-    id("net.ltgt.apt-idea") version "0.21"
-    id("net.ltgt.apt-eclipse") version "0.21"
+	id("org.rivierarobotics.gradlerioredux") version "0.8.0"
 }
 
 gradleRioRedux {
-    robotClass = "org.rivierarobotics.robot.Robot"
-    teamNumber = 5818
+	robotClass = "org.rivierarobotics.robot.Robot"
+	teamNumber = 5818
 }
 
 dependencies {
+	implementation("org.rivierarobotics.apparjacktus:apparjacktus:0.1.1")
 	commonLib("net.octyl.apt-creator", "apt-creator", "0.1.4") {
 		compileOnly(lib("annotations"))
 		annotationProcessor(lib("processor"))
 	}
-	commonLib("com.google.dagger", "dagger", "2.21") {
+	commonLib("com.google.dagger", "dagger", "2.25.4") {
 		implementation(lib())
 		annotationProcessor(lib("compiler"))
-	}
-	implementation("org.slf4j:slf4j-api:1.7.25")
-	commonLib("ch.qos.logback", "logback", "1.2.3") {
-		implementation(lib("classic"))
-		implementation(lib("core"))
 	}
 }
 
